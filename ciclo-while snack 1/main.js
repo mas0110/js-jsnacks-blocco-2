6046a75accebc55b9f5440f76d936c6d9763a3a3
mas0110 <masoudmohammed13@gmail.com>
2024-01-12 17:09:31 +0100

    ciclo while snack 1

--- ciclo-while snack 1/main.js
+++ ciclo-while snack 1/main.js
@@ -1,1 +1,15 @@
-// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
+
+const  arrayvuota= [ ]
+
+let i = 0
+
+while( i< 50){
+    let numeroUtente = parseInt(prompt("inserisci un numero"))
+
+    arrayvuota.push(numeroUtente)
+    i += numeroUtente
+}
+
+console.log("array", arrayvuota)
+console.log("somma degli elementi", i)
