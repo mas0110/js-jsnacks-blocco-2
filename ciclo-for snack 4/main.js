fd0b675f364ecb0f43c7c268c453d232feed027a
mas0110 <masoudmohammed13@gmail.com>
2024-01-15 12:34:16 +0100

    ciclo-for snack 4

--- ciclo-for snack 4/main.js
+++ ciclo-for snack 4/main.js
@@ -1,1 +1,16 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+
+let NumeroUtente = parseInt(prompt("Inserisci un numero:"));
+
+
+for (let  i = 0; i < NumeroUtente; i++) {
+
+    if (NumeroUtente % 2 === 0) {
+        alert("Il numero inserito è pari:", NumeroUtente)
+        console.log("Il numero inserito è pari:", NumeroUtente);
+    } else {
+        alert("Il numero inserito è dispari. Il numero successivo è:", NumeroUtente + 1)
+        console.log("Il numero inserito è dispari. Il numero successivo è:", NumeroUtente + 1);
+    }
+}
