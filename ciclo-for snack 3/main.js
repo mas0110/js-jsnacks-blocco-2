9310ed8850f98cc99c921c9ea5695a7ee403efaa
mas0110 <masoudmohammed13@gmail.com>
2024-01-15 12:33:47 +0100

    ciclo-for snack 3

--- ciclo-for snack 3/main.js
+++ ciclo-for snack 3/main.js
@@ -1,1 +1,29 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+
+
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [10, 20, 30];
+
+function randomnumber( min,max){
+    return Math.floor(math.random()*(max-min +1)+min)
+}
+
+let i = 0
+
+let arrayminore;
+
+if (array1.length > array2.length){
+    arrayminore = array2;
+} else{
+    arrayminore = array1;
+}
+
+for ( let i = 0; array1.length !== array2; i++){
+
+    let inseriscinumero = randomnumber (1,100)
+
+    arrayminore.push(inseriscinumero)
+}
+
+console.log( array1, array2 )
+
